6dc24dbbd3411411f1e4fa1a4b8068246448768a
Adi-Sauter <adriansauter07.As@gmail.com>
2021-07-04 13:27:45 +0200

    Update

---
+++ Assets/Scripts/PointerClick.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PointerClick : MonoBehaviour
+{
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    // public void OnBeginDrag(PointerEventData eventData)
+    // {
+    //     Debug.Log("Drag Begin");
+    // }
+
+    // public void OnDrag(PointerEventData eventData)
+    // {
+    //     Debug.Log("Dragging");
+    // }
+
+    // public void OnEndDrag(PointerEventData eventData)
+    // {
+    //     Debug.Log("Drag Ended");
+    // }
+
+    // public void OnPointerClick(PointerEventData eventData)
+    // {
+    //     Debug.Log("Clicked: " + eventData.pointerCurrentRaycast.gameObject.name);
+    // }
+
+    // public void OnPointerDown(PointerEventData eventData)
+    // {
+    //     Debug.Log("Mouse Down: " + eventData.pointerCurrentRaycast.gameObject.name);
+    // }
+
+    // public void OnPointerEnter(PointerEventData eventData)
+    // {
+    //     Debug.Log("Mouse Enter");
+    // }
+
+    // public void OnPointerExit(PointerEventData eventData)
+    // {
+    //     Debug.Log("Mouse Exit");
+    // }
+
+    // public void OnPointerUp(PointerEventData eventData)
+    // {
+    //     Debug.Log("Mouse Up");
+    // }
+
+    public Slider slider;
+
+    void OnEnable()
+    {
+    //Subscribe to the Slider Click event
+    slider.onValueChanged.AddListener(delegate { sliderCallBack(slider.value); });
+    }
+
+    //Will be called when Slider changes
+    void sliderCallBack(float value)
+    {
+    Debug.Log("Slider Changed: " + value);
+    slider.GetComponent<Image>().color = Color.cyan;
+    slider.GetComponentInChildren<MeshRenderer>().material.color = Color.yellow;
+    }
+
+    void OnDisable()
+    {
+    //Un-Subscribe To Slider Event
+    slider.onValueChanged.RemoveListener(delegate { sliderCallBack(slider.value); });
+    }
+
+    // public void OnPointerDown(PointerEventData eventData)
+    // {
+    //     Debug.Log("Click");
+    //     //this.GetComponent<Image>().color = Color.cyan;
+    //     //this.GetComponentInChildren<MeshRenderer>().material.color = Color.yellow;
+    // }
+
+}
